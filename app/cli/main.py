7c396c1c981da77f18e197b2fe246afb9cac2d69
Felipe Martín Rodríguez <felipemrz01@gmail.com>
2025-08-16 15:14:43 +0200

    feat: implement cli commands

---
+++ app/cli/main.py
@@ -0,0 +1,37 @@
+import typer
+from typing import Optional
+
+from app.api import get_all_routers
+
+app = typer.Typer()
+
+
+@app.command()
+def start(port: int = typer.Option(8000, "--port", "-p", help="Port to listen on"),
+          host:str = typer.Option("0.0.0.0", "--host", "-h", help="Host to listen on"),
+          reload: Optional[bool] = typer.Option(False, "--reload", "-r",
+                                                help="Reload the server on file changes")):
+    """
+    Start the FastAPI server.
+
+    Starts the FastAPI server with the specified host, port, and reload option.
+
+    Args:
+    port (int, optional): Port to listen on. Defaults to 8000.
+    host (str, optional): Host to bind to. Defaults to "0.0.0.0".
+    reload (bool, optional): Reload the server on file changes. Defaults to False
+    """
+
+    import uvicorn
+    from app.main import app as fastapi_app
+
+    for router in get_all_routers():
+        fastapi_app.include_router(router)
+
+    typer.echo(f"Starting FastAPI server on http://{host}:{port}")
+    uvicorn.run(fastapi_app, host=host, port=port, reload=reload)
+
+
+if __name__ == "__main__":
+    app()
+
